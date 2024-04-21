09aea68dc56e6e85939bab1c75aa34de3a9eb420
diexban <166546790+diexban@users.noreply.github.com>
2024-04-21 14:56:20 +0200

    Add files via upload
    Script to be run in MySQL Workbench

---
+++ Diego Trejos - Fast Track Technical Test MySQL.sql
@@ -0,0 +1,5 @@
+SELECT * FROM Users_With_Three_or_More_Deposits;
+SELECT * FROM Users_With_One_Withdrawal;
+SELECT * FROM Top_3_Users_Highest_Deposits;
+SELECT * FROM All_Deposits_For_Users;
+SELECT * FROM User_Balances;
